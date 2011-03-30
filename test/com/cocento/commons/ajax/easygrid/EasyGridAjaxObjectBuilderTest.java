95005fff32316862fc8e3cbb060093f364363ebb
Bruno Alvares da Costa <brunoadacosta@gmail.com>
2011-03-29 21:46:48 -0300

    Corrigindo teste :P

--- test/com/cocento/commons/ajax/easygrid/EasyGridAjaxObjectBuilderTest.java
+++ test/com/cocento/commons/ajax/easygrid/EasyGridAjaxObjectBuilderTest.java
@@ -16,7 +16,7 @@ public class EasyGridAjaxObjectBuilderTest {
 	private static final ClassA a = new ClassA();
 	private static final ClassB b = new ClassB();
 	private static final Gson gson = new GsonBuilder().serializeNulls().create();
-	private static final String cleanJson = "{\"page\":1,\":0,\"records\":0,\"rows\":[id:\"\", cell[]]}";
+	private static final String cleanJson = "{\"page\":1,\"records\":0,\"rows\":[]}";
 
 	@BeforeClass
 	public static void setUpBefore() {
@@ -46,7 +46,7 @@ public class EasyGridAjaxObjectBuilderTest {
 
 		String json = gson.toJson(obj);
 
-		assertEquals("Json esperado est· incorreto.", cleanJson, json);
+		assertEquals("Json esperado est√° incorreto.", cleanJson, json);
 	}
 
 	@Test(expected = IllegalArgumentException.class)
